51df2162ffc47b928141f41bf00c1f2bdf65dfa6
OscarNii <immanuelsowahoscar@gmail.com>
2023-09-12 10:48:16 +0000

    Update the background image of the second slide from bleach.jpeg to naru.png

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ document.getElementById('next').onclick = function(){
 
 document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
-    document.getElementById('slide').appendChild(lists[0]);
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
     
 }
